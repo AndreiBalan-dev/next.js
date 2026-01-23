f4c32f1a6a3c41101aca24cc1b1ef25f8f636791
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-23 16:54:54 +0100

    Don't use build id with Pages data routes

--- packages/next/src/server/lib/router-utils/filesystem.ts
+++ packages/next/src/server/lib/router-utils/filesystem.ts
@@ -574,7 +574,10 @@ export async function setupFsCheck(opts: {
           continue
         }
 
-        const nextDataPrefix = `/_next/data/${buildId}/`
+        // When deploymentId is set, data URLs don't include the build ID
+        const nextDataPrefix = opts.config.deploymentId
+          ? '/_next/data/'
+          : `/_next/data/${buildId}/`
 
         if (
           type === 'pageFile' &&
@@ -582,7 +585,7 @@ export async function setupFsCheck(opts: {
           curItemPath.endsWith('.json')
         ) {
           items = nextDataRoutes
-          // remove _next/data/<build-id> prefix
+          // remove _next/data/ or _next/data/<build-id>/ prefix
           curItemPath = curItemPath.substring(nextDataPrefix.length - 1)
 
           // remove .json postfix
