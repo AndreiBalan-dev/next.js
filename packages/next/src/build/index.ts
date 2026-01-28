ce9369cc99ef13efcde45632b0196a52ba2843d0
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-28 13:36:57 +0100

    fixup

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -2008,7 +2008,7 @@ export default async function build(
                 : []),
               // When deploymentId is available, headers are used for softnavs, and the build id
               // isn't needed anymore.
-              ...(config.deploymentId ? [BUILD_ID_FILE] : []),
+              ...(!config.deploymentId ? [BUILD_ID_FILE] : []),
               path.join(SERVER_DIRECTORY, NEXT_FONT_MANIFEST + '.js'),
               path.join(SERVER_DIRECTORY, NEXT_FONT_MANIFEST + '.json'),
               SERVER_FILES_MANIFEST + '.json',
