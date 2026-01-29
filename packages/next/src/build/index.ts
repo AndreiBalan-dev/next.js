98686e7a5cd3a6373b66b04ec32f0bb28a8ae537
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2026-01-28 16:34:41 -0800

    fix: fully static pages should emit & serve static rsc payloads

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -3345,6 +3345,19 @@ export default async function build(
                     ? 404
                     : meta.status
 
+                const canUseStaticPrefetchDataRoute =
+                  isRoutePPREnabled &&
+                  metadata?.postponed == null &&
+                  dataRoute &&
+                  existsSync(
+                    path.join(
+                      distDir,
+                      'server',
+                      'app',
+                      dataRoute.replace(/^\//, '')
+                    )
+                  )
+
                 prerenderManifest.routes[route.pathname] = {
                   initialStatus: status,
                   initialHeaders: meta.headers,
@@ -3359,7 +3372,9 @@ export default async function build(
                   initialExpireSeconds: cacheControl.expire,
                   srcRoute: page,
                   dataRoute,
-                  prefetchDataRoute: undefined,
+                  prefetchDataRoute: canUseStaticPrefetchDataRoute
+                    ? dataRoute
+                    : undefined,
                   allowHeader: ALLOWED_HEADERS,
                 }
               } else {
